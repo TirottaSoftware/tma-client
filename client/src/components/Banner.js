79ad57db77bece3fbd682723a230af5f9e7f2628
Cristian Tirotta <59051220+TirottaSoftware@users.noreply.github.com>
2022-11-23 16:23:44 +0200

    Updated Banner buttons

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 function Banner({ movie, add }) {
+
+    const nav = useNavigate()
+
     return (
         <div className='banner'>
             <div className='banner-img'>
                 <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt="Banner Image" />
                 <div className='banner-content'>
                     <h2>{movie.title}</h2>
-                    <div>
-                        <button className='btn-more'>More</button>
+                    <div className='btn-container'>
+                        <button onClick={() => nav(`/movie/${movie.id}`)} className='btn-more'>More</button>
                         <button onClick={() => add(movie)} className='btn-add'>+</button>
                     </div>
                 </div>
